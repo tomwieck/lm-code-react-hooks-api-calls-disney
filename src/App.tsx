fb6c6e8028eb98cd5e6b41bc96f5b7b8d8f3f10e
Tom <admin@Admins-MacBook-Pro.local>
2023-02-07 11:25:41 +0000

    Complete up to activity 3, using useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/header';
 import CharacterContainer from './components/character_container';
 import Navigation from './components/navigation';
@@ -11,24 +11,40 @@ const App : React.FC = () => {
 	const [currentPage, setCurrentPage] = useState<number>(1);
 
   // Some dummy state representing disney characters
-  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
-    {
-      _id: 6,
-      name: "'Olu Mel",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png"
-    },
-    {
-      _id: 25,
-      name: "Abu",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/3/3f/Profile_-_Abu.png"
-    },
-    {
-      _id: 30,
-      name: "Ace",
-      imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
-    },
-  ]);
+  // const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
+  //   {
+  //     _id: 6,
+  //     name: "'Olu Mel",
+  //     imageUrl: "https://static.wikia.nocookie.net/disney/images/6/61/Olu_main.png"
+  //   },
+  //   {
+  //     _id: 25,
+  //     name: "Abu",
+  //     imageUrl: "https://static.wikia.nocookie.net/disney/images/3/3f/Profile_-_Abu.png"
+  //   },
+  //   {
+  //     _id: 30,
+  //     name: "Ace",
+  //     imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
+  //   },
+  // ]);
 
+  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
+
+  const getCharacters = async (pageNumber : number) => {
+    const apiResponse = await fetch(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
+    const json = await apiResponse.json() as { data: DisneyCharacter[] };
+      setCharacters(json.data);
+  };
+
+  useEffect(() => {
+    getCharacters(1);
+  }, []);
+
+
+  useEffect(() => {
+    getCharacters(currentPage);
+  }, [currentPage]);
   
 
   return (
